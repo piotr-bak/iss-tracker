e832a3eef7226b072a56ce2f3a4f5edab77b0f91
Casahel <68067947+Casahel@users.noreply.github.com>
2023-09-19 13:36:39 +0200

    feat(types): added TypeScript support for useFetch

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import serverErrorCodes from "../utils/serverErrorCodes.ts";
+
+type FetchArguments = {
+    url: string;
+    options?: RequestInit;
+    logErrors: boolean;
+};
+type FetchResult<T> = {
+    data: T | undefined;
+    isError: boolean;
+    isLoading: boolean;
+};
+export function useFetch<T>({
+    url,
+    options,
+    logErrors = true,
+}: FetchArguments): FetchResult<T> {
+    const [data, setData] = useState<T | undefined>(undefined);
+    const [isError, setIsError] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        setIsLoading(true);
+        setData(undefined);
+        setIsError(false);
+
+        const controller = new AbortController();
+
+        (async () => {
+            try {
+                const response = await fetch(url, {
+                    signal: controller.signal,
+                    ...options,
+                });
+                if (!response.ok) {
+                    const message = `Invalid server response. Error ${
+                        response.status
+                    }: ${serverErrorCodes.get(response.status)}`;
+                    throw new Error(message);
+                } else {
+                    const json = await response.json();
+                    setData(json);
+                }
+            } catch (error) {
+                if (error instanceof Error) {
+                    if ("AbortError" === error.name) return;
+                    setIsError(true);
+                    if (logErrors) console.error(error.message);
+                }
+            } finally {
+                setIsLoading(false);
+            }
+        })();
+        return () => controller.abort();
+    }, [url]);
+
+    return { data, isError, isLoading };
+}
