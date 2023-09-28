f42fbbe095a6cc32fc76ff836a9cb42ebc4df8b8
Casahel <68067947+Casahel@users.noreply.github.com>
2023-09-28 08:19:47 +0200

    chore: code not needed at the moment moved to EOF

--- src/components/Planet/Planet.tsx
+++ src/components/Planet/Planet.tsx
@@ -1,11 +1,8 @@
 import { useState, useMemo, useEffect, useRef, SetStateAction } from "react";
 import * as satellite from "satellite.js";
-// import booleanPointInPolygon from "@turf/boolean-point-in-polygon";
-// import * as turf from "@turf/helpers";
 import * as THREE from "three";
 import Globe from "react-globe.gl";
 import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader.js";
-//import { useFetch } from "../../hooks/useFetch.ts";
 
 type RingSettings = {
     lat: number;
@@ -20,15 +17,13 @@ const SAT_SIZE = 80;
 
 export function Planet() {
     //const { data, isError, isLoading } = useFetch();
-    const globeEl = useRef();
+    const globeRef = useRef();
     const [time, setTime] = useState(new Date());
     const [globeRadius, setGlobeRadius] = useState("");
     const [ringsData, setRingsData] = useState<RingSettings[]>();
-    const [countries, setCountries] = useState({ features: [] });
     const [issModel, setIssModel] = useState<THREE.Group | undefined>(
         undefined
     );
-    const [altitude, setAltitude] = useState(0.1);
     const [transitionDuration, setTransitionDuration] = useState(1000);
 
     const TLE = `1 25544U 98067A   23262.35054104  .00014745  00000-0  27129-3 0  9992
@@ -40,44 +35,25 @@ export function Planet() {
     const satrec = satellite.twoline2satrec(
         ...(splitTLE(TLE) as [string, string])
     );
-
     useEffect(() => {
         setInterval(() => {
-            setTime((time) => new Date(+time + 500));
-        }, 500);
+            setTime((time) => new Date(+time + 333));
+        }, 333);
     }, []);
 
     useEffect(() => {
-        (async () => {
-            try {
-                const response = await fetch(
-                    "../src/data/ne_110m_admin_0_countries.geojson"
-                );
-                const json = await response.json();
-                setCountries(json);
-                console.log("Data loaded sucessfully");
-            } catch (err) {
-                console.error(err);
-            }
-        })();
+        const loader = new FBXLoader();
+        loader.load("../../src/assets/models/iss/iss.fbx", (model) => {
+            model.scale.set(1, 1, 1);
+            setIssModel(model);
+        });
     }, []);
-    // useEffect(() => {
-    //     // load data
-    //     fetch("../src/data/ne_110m_admin_0_countries.geojson")
-    //         .then((res) => res.json())
-    //         .then((countries) => {
-    //             setCountries(countries);
 
-    //             setTimeout(() => {
-    //                 setAltitude(0.1);
-    //             }, 3000);
-    //         });
-    // }, []);
+    useEffect(() => {
+        setGlobeRadius(globeRef.current.getGlobeRadius());
+        globeRef.current.pointOfView({ altitude: 3.5 });
+    }, []);
 
-    // fetch("../../data/ne_110m_admin_0_countries.geojson")
-    // .then((res) => res.json())
-    // .then(setGeoData)
-    // .catch((err) => console.error("Something went wrong. Error:", err));
     const satelliteData = useMemo(() => {
         const gmst = satellite.gstime(time);
         const eci = satellite.propagate(satrec, time);
@@ -107,67 +83,13 @@ export function Planet() {
         //checkTurf(lng, lat);
     }
 
-    // function checkTurf(lng, lat) {
-    //     if (!countries) return;
-    //     const satellitePosition = turf.point([lng, lat]);
-    //     console.log(satellitePosition);
-    //     // for (const feature of countries.features) {
-    //     //     const countryPolygon = turf.multiPolygon([
-    //     //         [[feature.geometry.coordinates]],
-    //     //     ]);
-    //     //     if (booleanPointInPolygon(satellitePosition, countryPolygon)) {
-    //     //         console.log("Success");
-    //     //         //console.log(feature.properties.FORMAL_EN);
-    //     //         break;
-    //     //     } else {
-    //     //         console.log("not success");
-    //     //         break;
-    //     //     }
-    //     // }
-    // }
-    // function checkTurf(lng, lat) {
-    //     // const satellitePoint = point([lng, lat]);
-    //     // console.log(satellitePoint);
-
-    //     // for (const feature of countries.features) {
-    //     //     const countryPolygon = polygon(feature.geometry);
-    //     //     console.log(feature);
-    //     // }
-    //     // if (countries) {
-    //     for (const feature of countries.features) {
-    //         const countryPolygon = polygon(feature);
-    //         const satellitePosition = point([lng, lat]);
-    //         console.log(countryPolygon);
-    //         //console.log(satellitePosition);
-    //         // if (booleanPointInPolygon(satellitePosition, countryPolygon)) {
-    //         //     console.log(true);
-    //         //     break;
-    //         // }
-    //     }
-    //     // }
-    //     return null;
-    // }
-
     const colorInterpolator = (t: number) =>
         `rgba(70,130,180,${Math.sqrt(1 - t)})`;
-
-    useEffect(() => {
-        const loader = new FBXLoader();
-        loader.load("../../src/assets/models/iss/iss.fbx", (model) => {
-            model.scale.set(1, 1, 1);
-            setIssModel(model);
-        });
-    }, []);
-
-    useEffect(() => {
-        setGlobeRadius(globeEl.current.getGlobeRadius());
-        globeEl.current.pointOfView({ altitude: 3.5 });
-    }, []);
 
     return (
         <div className='globe-wrapper'>
             <Globe
-                ref={globeEl}
+                ref={globeRef}
                 globeImageUrl='//unpkg.com/three-globe/example/img/earth-night.jpg'
                 backgroundImageUrl='//unpkg.com/three-globe/example/img/night-sky.png'
                 objectsData={satelliteData}
@@ -182,17 +104,99 @@ export function Planet() {
                 ringPropagationSpeed='propagationSpeed'
                 ringRepeatPeriod='repeatPeriod'
                 ringAltitude={0.02}
-                //polygonsData={countries.features}
-                polygonAltitude={0.001}
-                polygonCapColor={() => "rgba(0, 0, 0, 0.0)"}
-                polygonSideColor={() => "rgba(0, 0, 0, 0.0)"}
-                polygonStrokeColor={() => "rgba(70, 130, 180, .2)"}
-                polygonLabel={({ properties: d }) => `
-                  <b>${d.ADMIN} (${d.ISO_A2})</b> <br />
-                  Population: <i>${Math.round(+d.POP_EST / 1e4) / 1e2}M</i>
-                `}
-                polygonsTransitionDuration={transitionDuration}
             />
         </div>
     );
 }
+
+// useEffect(() => {
+//   (async () => {
+//       try {
+//           const response = await fetch(
+//               "../src/data/ne_110m_admin_0_countries.geojson"
+//           );
+//           const json = await response.json();
+//           setCountries(json);
+//           console.log("Data loaded sucessfully");
+//       } catch (err) {
+//           console.error(err);
+//       }
+//   })();
+// }, []);
+
+// const [countries, setCountries] = useState({ features: [] });
+// const [altitude, setAltitude] = useState(0.1);
+
+// import booleanPointInPolygon from "@turf/boolean-point-in-polygon";
+// import * as turf from "@turf/helpers";
+
+// useEffect(() => {
+//     // load data
+//     fetch("../src/data/ne_110m_admin_0_countries.geojson")
+//         .then((res) => res.json())
+//         .then((countries) => {
+//             setCountries(countries);
+
+//             setTimeout(() => {
+//                 setAltitude(0.1);
+//             }, 3000);
+//         });
+// }, []);
+
+// fetch("../../data/ne_110m_admin_0_countries.geojson")
+// .then((res) => res.json())
+// .then(setGeoData)
+// .catch((err) => console.error("Something went wrong. Error:", err));
+
+// function checkTurf(lng, lat) {
+//     if (!countries) return;
+//     const satellitePosition = turf.point([lng, lat]);
+//     console.log(satellitePosition);
+//     // for (const feature of countries.features) {
+//     //     const countryPolygon = turf.multiPolygon([
+//     //         [[feature.geometry.coordinates]],
+//     //     ]);
+//     //     if (booleanPointInPolygon(satellitePosition, countryPolygon)) {
+//     //         console.log("Success");
+//     //         //console.log(feature.properties.FORMAL_EN);
+//     //         break;
+//     //     } else {
+//     //         console.log("not success");
+//     //         break;
+//     //     }
+//     // }
+// }
+// function checkTurf(lng, lat) {
+//     // const satellitePoint = point([lng, lat]);
+//     // console.log(satellitePoint);
+
+//     // for (const feature of countries.features) {
+//     //     const countryPolygon = polygon(feature.geometry);
+//     //     console.log(feature);
+//     // }
+//     // if (countries) {
+//     for (const feature of countries.features) {
+//         const countryPolygon = polygon(feature);
+//         const satellitePosition = point([lng, lat]);
+//         console.log(countryPolygon);
+//         //console.log(satellitePosition);
+//         // if (booleanPointInPolygon(satellitePosition, countryPolygon)) {
+//         //     console.log(true);
+//         //     break;
+//         // }
+//     }
+//     // }
+//     return null;
+// }
+/* Globe component props:
+                //polygonsData={countries.features}
+                // polygonAltitude={0.001}
+                // polygonCapColor={() => "rgba(0, 0, 0, 0.0)"}
+                // polygonSideColor={() => "rgba(0, 0, 0, 0.0)"}
+                // polygonStrokeColor={() => "rgba(70, 130, 180, .2)"}
+                // polygonLabel={({ properties: d }) => `
+                //   <b>${d.ADMIN} (${d.ISO_A2})</b> <br />
+                //   Population: <i>${Math.round(+d.POP_EST / 1e4) / 1e2}M</i>
+                // `}
+                // polygonsTransitionDuration={transitionDuration}
+                */
