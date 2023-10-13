93a54a40444e27a265769ac292a40f0e692d9785
Casahel <68067947+Casahel@users.noreply.github.com>
2023-10-13 12:54:19 +0200

    types: Added types to constants and helper funct.

---
+++ src/constants/earthConstants.ts
@@ -0,0 +1,2 @@
+export const EARTH_RADIUS: number = 6371;
+export const MODEL_SCALE: number = 1;
--- src/utils/satelliteHelpers.ts
+++ src/utils/satelliteHelpers.ts
@@ -5,11 +5,11 @@ export function splitTLE(twoLineElement: string): string[] {
     return twoLineElement.split("\n").map((line) => line.trim());
 }
 
-export function degreesToRadians(deg) {
+export function degreesToRadians(deg: number): number {
     return deg * (Math.PI / 180);
 }
 
-export function geoToSpherical(lat, lon, alt = 0) {
+export function geoToSpherical(lat: number, lon: number, alt: number = 0) {
     const phi = degreesToRadians(lat);
     const theta = degreesToRadians(lon);
     const radius = ((EARTH_RADIUS + alt) / EARTH_RADIUS) * MODEL_SCALE;
@@ -21,7 +21,11 @@ export function geoToSpherical(lat, lon, alt = 0) {
     };
 }
 
-export function calcSatPosition(lat, lon, alt) {
+export function calcSatPosition(
+    lat: number,
+    lon: number,
+    alt: number
+): THREE.Vector3 {
     const { phi, theta, radius } = geoToSpherical(lon, lat, alt);
     const x = Math.cos(phi) * Math.cos(theta) * radius;
     const y = Math.sin(phi) * radius;
@@ -29,7 +33,7 @@ export function calcSatPosition(lat, lon, alt) {
     return new THREE.Vector3(x, y, z);
 }
 
-export function calcSatRotation(lat, lon) {
+export function calcSatRotation(lat: number, lon: number): THREE.Euler {
     const { phi, theta } = geoToSpherical(lat, lon);
     const x = 0;
     const y = theta;
