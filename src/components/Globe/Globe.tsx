5248964b6fe428f38fa28242d2487a5c9fcb52f5
Casahel <68067947+Casahel@users.noreply.github.com>
2023-10-14 23:48:26 +0200

    feat: Light emissions added to the Globe

--- src/components/Globe/Globe.tsx
+++ src/components/Globe/Globe.tsx
@@ -4,16 +4,23 @@ import { useLoader } from "@react-three/fiber";
 import { Sphere } from "@react-three/drei";
 import * as THREE from "three";
 import textureEarthDay from "/assets/textures/8k_earth_daymap.webp";
+import textureEmissiveMap from "/assets/textures/earth_lights.gif";
 import { MODEL_SCALE } from "../../constants/earthConstants.ts";
 
 export function Globe() {
-    const base = useLoader(THREE.TextureLoader, textureEarthDay);
+    const baseMap = useLoader(THREE.TextureLoader, textureEarthDay);
+    const lightsMap = useLoader(THREE.TextureLoader, textureEmissiveMap);
     const ref = useRef<THREE.Mesh<THREE.SphereGeometry>>(null);
 
     return (
         <>
             <Sphere castShadow ref={ref} args={[MODEL_SCALE, 128, 64]}>
-                <meshStandardMaterial map={base} />
+                <meshStandardMaterial
+                    map={baseMap}
+                    emissiveMap={lightsMap}
+                    emissive={"#EBCD84"}
+                    emissiveIntensity={0.8}
+                />
             </Sphere>
             <Satellite />
         </>
