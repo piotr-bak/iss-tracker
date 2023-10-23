8dac61bf724f3bb99ab3e745ecc53b2b3c4cb474
Casahel <68067947+Casahel@users.noreply.github.com>
2023-10-23 21:38:17 +0200

    style: Infopanel styling

--- src/components/Infopanel/Infopanel.tsx
+++ src/components/Infopanel/Infopanel.tsx
@@ -5,15 +5,17 @@ export function Infopanel() {
     const info = useSatDataContext();
     return (
         <div className={styles.panel}>
-            <h1>ISS Tracker</h1>
+            <div className={styles.header}>
+                <h1>ISS Tracker</h1>
+            </div>
             <div className={styles.infobox}>
-                <p>Current ISS position:</p>
+                <p>Current {info.satData.satName} position:</p>
                 <p>
                     Longitude: {info.satData.lon.toFixed(3)}
                     <br />
-                    Latitude: {info.satData.alt.toFixed(3)}
+                    Latitude: {info.satData.lat.toFixed(3)}
                     <br />
-                    Altitude: {info.satData.lat.toFixed(3)} km
+                    Altitude: {info.satData.alt.toFixed(2)} km
                 </p>
             </div>
         </div>
