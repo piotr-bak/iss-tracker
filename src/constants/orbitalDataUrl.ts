afeb2c898de922460a98c7580389589f5598b537
Casahel <68067947+Casahel@users.noreply.github.com>
2023-10-24 15:34:36 +0200

    feat: added alternative TLE source address

--- src/constants/orbitalDataUrl.ts
+++ src/constants/orbitalDataUrl.ts
@@ -1,2 +1,3 @@
-export const ORBITAL_DATA_URL: string =
-    "https://celestrak.org/NORAD/elements/gp.php?CATNR=25544&FORMAT=TLE";
+export const ORBITAL_DATA_URL: string = "https://live.ariss.org/iss.txt";
+
+//Alternatively: "https://celestrak.org/NORAD/elements/gp.php?CATNR=25544&FORMAT=TLE";
