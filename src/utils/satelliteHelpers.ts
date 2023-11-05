57fb57248b3fb4148d8aac7f42898eb7d084989f
Casahel <68067947+Casahel@users.noreply.github.com>
2023-11-05 23:37:22 +0100

    refactor: imports

--- src/utils/satelliteHelpers.ts
+++ src/utils/satelliteHelpers.ts
@@ -1,12 +1,12 @@
-import * as THREE from "three";
+import { Euler, Vector3 } from "three";
 import { EARTH_RADIUS, MODEL_SCALE } from "../constants/earthConstants.ts";
 
 export function splitTLE(tleData: string) {
-    // if (typeof tleData !== "string") {
-    //     throw new Error(
-    //         `TLE data is not a string. Received ${typeof tleData} instead.`
-    //     );
-    // }
+    if (typeof tleData !== "string") {
+        throw new Error(
+            `TLE data is not a string. Received ${typeof tleData} instead.`
+        );
+    }
     const lines = tleData.split("\n").map((line) => line.trim());
     if (lines.length >= 2) {
         // Returns the 1st line as the satellite name and the 2nd and 3rd line as the orbit data
@@ -44,10 +44,10 @@ export function calcSatPosition(
     const x = Math.cos(phi) * Math.cos(theta) * radius;
     const y = Math.sin(phi) * radius;
     const z = -Math.cos(phi) * Math.sin(theta) * radius;
-    return new THREE.Vector3(x, y, z);
+    return new Vector3(x, y, z);
 }
 
-export function calcSatRotation(lat: number, lon: number): THREE.Euler {
+export function calcSatRotation(lat: number, lon: number): Euler {
     const { phi, theta } = geoToSpherical(lat, lon);
 
     //Magic numbers below are for visual purposes only - so the ISS looks better
@@ -56,5 +56,5 @@ export function calcSatRotation(lat: number, lon: number): THREE.Euler {
     const x = 2; //Pitch, default: 0;
     const y = theta * Math.PI; //Yaw, default: theta;
     const z = phi * Math.PI * 1.2; //Roll, default: phi * Math.PI;
-    return new THREE.Euler(x, y, z);
+    return new Euler(x, y, z);
 }
