042ecc56e41d673d922f7e4aaa04d420d3cf6753
Casahel <68067947+Casahel@users.noreply.github.com>
2023-11-05 23:39:32 +0100

    docs: short description of Context's purpose added

--- src/contexts/SatelliteDataContext.tsx
+++ src/contexts/SatelliteDataContext.tsx
@@ -2,6 +2,7 @@ import { createContext, ReactNode, useContext, useState } from "react";
 import { SatContextType } from "../types/ContextsTypes.ts";
 import { SATELLITE_DATA_OBJECT } from "../constants/satelliteDataObject.ts";
 
+//This Context is used to pass the Sat orbital data into the infobox
 export const SatDataContext = createContext<SatContextType>({
     satData: SATELLITE_DATA_OBJECT,
     setSatData: () => {},
