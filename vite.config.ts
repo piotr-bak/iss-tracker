e9302580a65fd6397713fe683e3287103ab625a7
piotr-bak <68067947+piotr-bak@users.noreply.github.com>
2024-03-19 12:26:10 +0100

    fix: updated config for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import sass from "sass";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig( {
+    base: '/iss-tracker/',
     plugins: [react()],
     css: {
         preprocessorOptions: {
